f62a60cae064d099311930887a71c33d7464454e
William J Teskey <willjteskey@gmail.com>
2018-04-17 22:35:00 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -1,6 +1,6 @@
 
 def hopper
-	programmer_hash = 
+		programmer_hash = 
  		{
         :grace_hopper => {
           :known_for => "COBOL",
@@ -15,9 +15,10 @@ def hopper
           :languages => ["C"]
         }
      }
-
+          programmer_hash[:grace_hopper]
 
 end
+
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
@@ -37,6 +38,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+          programmer_hash[:alan_kay][:known_for]
+
 end
 
 def dennis_ritchies_language
@@ -55,6 +58,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+          programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -79,7 +83,9 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+ programmer_hash.merge(:yukihiro_matsumoto => {
+		 :known_for => "Ruby",
+		 :languages => ["LISP, C"]})
     
 end
 
@@ -102,6 +108,8 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
+      programmer_hash[:alan_kay][:known_for] = alans_new_info
+		 programmer_hash
      
 end
 
@@ -121,5 +129,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+		 programmer_hash[:dennis_ritchie][:languages].push(", Assembly")
+		 programmer_hash
 end

6edf207f706a8088fc7ceb0f059811c5cdc82f97
William J Teskey <willjteskey@gmail.com>
2018-04-17 22:34:13 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -108,7 +108,9 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-     
+      programmer_hash.merge(:yukihiro_matsumoto => {
+		 :known_for => "Ruby",
+		 :languages => ["LISP, C"]})
      
 end
 

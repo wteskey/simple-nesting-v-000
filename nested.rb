b1ab6ce4425b3a67a1be9b323dd36047ae891310
William J Teskey <willjteskey@gmail.com>
2018-04-17 22:28:00 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -1,19 +1,6 @@
 
 def hopper
-	programmer_hash = 
- 		{
-        :grace_hopper => {
-          :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
-        :alan_kay => {
-          :known_for => "Object Orientation",
-          :languages => ["Smalltalk", "LISP"]
-        },
-        :dennis_ritchie => {
-          :known_for => "Unix",
-          :languages => ["C"]
-        }
-      }
+	
 end
 
 
